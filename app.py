dd3ca5fd2e800331941f954d9c96616e22543ddb
Vigneshwaran BS <bs.vigneshwaran@gmail.com>
2025-04-17 14:02:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -152,6 +152,9 @@ def results(filename):
 # if __name__ == "__main__":
 #     app.run(debug=True)
 
+import os
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    host = os.getenv("FLASK_RUN_HOST", "127.0.0.1")   # override when needed
+    port = int(os.getenv("PORT", 5000))
+    app.run(host=host, port=port)
